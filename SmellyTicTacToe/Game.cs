b1ce537617dba6377c36317fa3e96dca17e3b8f3
Raúl Villares <raulvillares@gmail.com>
2022-01-22 16:58:07 +0100

    Housekeeping: move public method up

--- SmellyTicTacToe/Game.cs
+++ SmellyTicTacToe/Game.cs
@@ -55,6 +55,14 @@ namespace SmellyTicTacToe
             checkMovementIsValid(x, y);
             updateGameState(symbol, x, y);
         }
+        
+        public char Winner()
+        {   //if the positions in first row are taken
+            if (IsRowFullWithSameSymbol(0)) return _board.TileAt(0, 0).Symbol;
+            if (IsRowFullWithSameSymbol(1)) return _board.TileAt(1, 0).Symbol;
+            if (IsRowFullWithSameSymbol(2)) return _board.TileAt(2, 0).Symbol;
+            return ' ';
+        }    
 
         private void updateGameState(char symbol, int x, int y)
         {
@@ -94,14 +102,6 @@ namespace SmellyTicTacToe
         private bool isFirstMove()
         {
             return _lastSymbol == ' ';
-        }
-
-        public char Winner()
-        {   //if the positions in first row are taken
-            if (IsRowFullWithSameSymbol(0)) return _board.TileAt(0, 0).Symbol;
-            if (IsRowFullWithSameSymbol(1)) return _board.TileAt(1, 0).Symbol;
-            if (IsRowFullWithSameSymbol(2)) return _board.TileAt(2, 0).Symbol;
-            return ' ';
         }
 
         private bool IsRowFullWithSameSymbol(int row)
